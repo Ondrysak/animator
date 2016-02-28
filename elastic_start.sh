9adbfb204f75e7d5f0d580c01ba87448527a193b
Ondřej Naňka <nankaond@fit.cvut.cz>
2016-02-28 02:45:12 +0100

    elastic starting script added

---
+++ elastic_start.sh
@@ -0,0 +1,4 @@
+sudo su - elastic
+sudo xterm '/home/elastic/elasticsearch-2.1.1/bin/elasticsearch --conf.path /home/elastic/elasticsearch-2.1.1/config' & sudo xterm '/home/elastic/kibana-4.3.1-linux-x64/bin/kibana'
+exit
+
