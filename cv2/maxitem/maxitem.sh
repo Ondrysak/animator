e45d9e8098c113d974d47df5a983d715bde22c2d
Ondřej Naňka <nankaond@fit.cvut.cz>
2017-03-08 11:25:31 +0100

    added array manip, edited max item

--- cv2/maxitem/maxitem.sh
+++ cv2/maxitem/maxitem.sh
@@ -1,4 +1,4 @@
 function max_item {
- ls -l | tail -n+2 | tr -s " " | cut -d" " -f9 | grep "$1" | sed "s/${1}_//" | sort -n | tail -n1
+ ls -l | tail -n+2 | tr -s " " | cut -d" " -f9 | grep -E "^$1_[0-9]+$" | sed "s/${1}_//" | sort -n | tail -n1
 }
 max_item file_item 
