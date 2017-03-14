efdb87414f3e94f4f974cdc12cf35c96146de4c8
Ondřej Naňka <nankaond@fit.cvut.cz>
2017-03-14 18:36:40 +0100

    Replace maxitem.sh

--- cv2/maxitem/maxitem.sh
+++ cv2/maxitem/maxitem.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+#hyperpipe oneliner
 function max_item {
- ls -l | tail -n+2 | tr -s " " | cut -d" " -f9 | grep -E "^$1_[0-9]+$" | sed "s/${1}_//" | sort -n | tail -n1
+ ls -l | grep '^-' |tail -n+2 | tr -s " " | cut -d" " -f9 | grep -E "^$1_[0-9]+$" | sed "s/${1}_//" | sort -n | tail -n1
+}
+
+function max_item2 {
+  max=0
+  for file in *
+  do
+    temp=$(echo $file | grep -E "^$1_[0-9]+$" | sed "s/${1}_//")
+    [[ -f $file  ]] && [[ $max -lt $temp ]] && max=$temp
+  done
+  echo $max
 }
 max_item file_item 
+max_item2 file_item
