b5af1dc1f4626a15c41878e54bdb2aa99cc6ebd8
Ondřej Naňka <nankaond@fit.cvut.cz>
2017-03-28 15:13:49 +0200

    count

--- cv6/predmety/html.sh
+++ cv6/predmety/html.sh
@@ -3,8 +3,11 @@ codes=($(curl https://edux.fit.cvut.cz/courses/ | grep '\[DIR\]' | sed -Ee 's#<[
 rm -rf courses
 mkdir courses
 cd courses
+count=0
 for code in ${codes[*]}; do
   mkdir "$code"
-  echo "$( echo $code | sed -e 's/BI.-/BI-/' -e 's/MI.-/MI-/' )" > "$code"/info.txt
+  info="$( echo $code | sed -e 's/BI.-/BI-/' -e 's/MI.-/MI-/' )"
+  echo "$info">"$code"/info.txt
+  [[ $info == $code ]] && count=$(( $count + 1 ))
 done
-
+echo "Skutecnych predmetu je $count."
