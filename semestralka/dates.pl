1495a9f46398b69544d26ea7a922918da2fa43ec
Ondřej Naňka <nankaond@fit.cvut.cz>
2017-04-04 15:46:28 +0200

    added perl script to convert date based on timefmt string to unix epoch time

---
+++ semestralka/dates.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use DateTime::Format::Strptime;
+use POSIX qw(strftime);
+
+
+
+# (1) quit unless we have the correct number of command-line args
+$num_args = $#ARGV + 1;
+if ($num_args != 2) {
+    print "\nUsage: name.pl stringdatefmt date\n";
+    exit;
+}
+
+
+
+
+my $trp = DateTime::Format::Strptime->new(
+    pattern   => $ARGV[0],
+    locale    => 'cs_CZ',
+    time_zone => 'Europe/Prague',
+);
+
+
+my $dt = $trp->parse_datetime($ARGV[1]);
+
+print $dt->epoch, "\n";
