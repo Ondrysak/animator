2866d437572fea44a7743a1c2d3ec91f49dfc16d
Ondřej Naňka <nankaond@fit.cvut.cz>
2017-04-23 16:26:29 +0200

    parralle, bugfix subshell

--- semestralka/test.sh
+++ semestralka/test.sh
@@ -1,67 +1,70 @@
 #!/bin/bash
+set -o errtrace
 type exiftool || echo "you should install exiftool show mediafiles metadata"
 
-echo "#################################################################"
+for arg; do
+case "$arg" in
+
+1) echo "#################################################################"
 echo "Testing one valid input file with timeformat matching the default"
 echo "-v is used"
 echo "#################################################################"
--S 0.75
 ./animator -v sin_small.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots/anim.mp4 2>/dev/null;;
 
-echo "#################################################################"
+2) echo "#################################################################"
 echo "Testing two valid input files with timeformat matching the default"
 echo "-v is used"
 echo "#################################################################"
 
 ./animator -v sin_small.data sin_small2.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_1/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_1/anim.mp4 2>/dev/null;;
 
-echo "#################################################################"
+3) echo "#################################################################"
 echo "Testing four valid input files with timeformat matching the default"
 echo "-v is used"
 echo "#################################################################"
 
 ./animator -v sin_small.data sin_small2.data sin_small3.data sin_small4.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_2/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_2/anim.mp4 2>/dev/null;;
 
-echo "#################################################################"
+4) echo "#################################################################"
 echo "Testing two valid input files with timeformat matching the default"
 echo "-v is used"
 echo "-S 5 is used to set speed of reading datafile"
 echo "#################################################################"
 
 ./animator -S 5 -v sin_small.data sin_small2.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_3/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_3/anim.mp4 2>/dev/null;;
 
-echo "#################################################################"
+5) echo "#################################################################"
 echo "Testing two valid input files with timeformat matching the default"
 echo "-v is used"
 echo "-S 0.75 is used to set speed of reading datafile"
 echo "#################################################################"
 
 ./animator -S 0.75 -v sin_small.data sin_small2.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_4/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_4/anim.mp4 2>/dev/null;;
 
-echo "#################################################################"
+6) echo "#################################################################"
 echo "Testing two valid input files with timeformat matching the default"
 echo "-v is used"
 echo "-T 10 is used to set duration of the resulting video"
 echo "#################################################################"
 
 ./animator -T 10 -v sin_small.data sin_small2.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_5/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_5/anim.mp4 2>/dev/null;;
 
-echo "#################################################################"
+7) echo "#################################################################"
 echo "Testing two valid input files with timeformat matching the default"
 echo "-v is used"
 echo "-T 6.45 is used to set duration of the resulting video"
 echo "#################################################################"
 ./animator -T 6.45 -v sin_small.data sin_small2.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_6/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_6/anim.mp4 2>/dev/null;;
 
 
-echo "#################################################################"
+8) echo "#################################################################"
 echo "Testing two valid input files with timeformat matching the default"
 echo "-v is used"
 echo "-S 0.75 is used to set speed of reading datafile"
@@ -69,4 +72,19 @@ echo "-T 6.45 is used to set duration of the resulting video"
 echo "#################################################################"
 
 ./animator -S 0.75 -T 6.45 -v sin_small.data sin_small2.data
-exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_7/anim.mp4 2>/dev/null 
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_7/anim.mp4 2>/dev/null;; 
+
+
+9) echo "#################################################################"
+echo "Running two parralel instances of the script"
+echo "-v is used"
+echo "#################################################################"
+./animator -v sin_small.data &
+./animator -v sin_small.data &
+wait
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_8/anim.mp4 2>/dev/null
+exiftool -directory -filename -filetype -filesize -duration -videoframerate ./dots_9/anim.mp4 2>/dev/null;;
+
+?) echo "Invalid test number $arg" ;;
+esac
+done
