f6a03ef02d7acdb38659aece726b4b4bd2ace82b
nankaond <nankaond@fit.cvut.cz>
2017-05-18 12:17:02 +0200

    fixed depth

--- priprava/depth.sh
+++ priprava/depth.sh
@@ -6,19 +6,29 @@ procs=$(ps -eo pid,ppid | tr -s '[[:space:]]' | sed 's/^[[:space:]]*//' | tail -
 while read line; do
 pid=$( echo "$line" | cut -d' ' -f1 )
 ppid=$( echo "$line" | cut -d' ' -f2 )
-#echo "$pid and $ppid"
-if [[ "$ppid" -eq 0 ]]; then
-depth["$pid"]=0
-else
-parentdepth="${depth[$ppid]}"
-#echo "parentdepth $parentdepth"
-depth["$pid"]=$(( parentdepth + 1 ))
-fi	
+parent_arr["$pid"]="$ppid"
 done<<<"$procs"
-max=0
 
-for index in "${!depth[@]}"; do
-newdepth="${depth[$index]}"
+
+
+for proces in "${!parent_arr[@]}"; do
+	depth=0
+
+	parent="${parent_arr[$proces]}"
+
+	while [[ "$parent" != 0 ]]; do
+
+		parent="${parent_arr[$parent]}"
+		depth=$(( depth + 1 ))
+	done
+	depth_arr["$proces"]="$depth";
+done
+
+
+
+max=0
+for index in "${!depth_arr[@]}"; do
+newdepth="${depth_arr[$index]}"
 if [[ "$max" -lt "$newdepth" ]]; then
 result=()
 max="$newdepth"
