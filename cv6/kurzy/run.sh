82aa6af304852d623391e3256ae56266275c0e6c
nankaond <nankaond@fit.cvut.cz>
2017-10-26 13:25:46 +0200

    remove trash

--- cv6/kurzy/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -f script.sed -n data.html | sed '/^$/d' > mine.txt
-diff mine.txt data.txt && echo "OK"
--- priprava/while.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-filename=passwd
-while read line; do
-echo $line 
-echo hello
-done < /etc/"$filename"
